c883c00ba1fc820bbe532bae39e69493d59232a3
Prajjwalajit15 <prajjwalajit@gmail.com>
2023-11-03 17:52:56 +0530

    f3

--- options.js
+++ options.js
@@ -138,3 +138,114 @@ function onChangeBackgroundColor(bgColor){
         activeOptionstate.color = selectedColor;
     }
 }
+
+// Get the upload and download elements by their IDs
+const uploadButton = document.getElementById("upload");
+const downloadButton = document.getElementById("download");
+
+// Add event listener for the upload button
+uploadButton.addEventListener("click", () => {
+    // Implement the upload functionality here
+    alert("Do you want to upload this file ?");
+});
+
+// Add event listener for the download button
+downloadButton.addEventListener("click", () => {
+    // Implement the download functionality here
+    alert("Do you want to download this file ?");
+});
+
+// Add event listener for the upload button
+uploadButton.addEventListener("click", () => {
+    // Create an input element of type "file" to trigger file selection
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+
+    // Trigger a click event on the file input
+    fileInput.click();
+
+    // Handle the selected file when it changes
+    fileInput.addEventListener("change", (event) => {
+        const selectedFile = event.target.files[0];
+        // You can now work with the selected file (e.g., upload it to a server).
+        console.log("Selected file:", selectedFile);
+    });
+});
+
+// Add event listener for the download button
+downloadButton.addEventListener("click", () => {
+    // Implement the download functionality, e.g., generating a download link for data.
+    const dataToDownload = "This is the data to download.";
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = "data:text/plain;charset=utf-8," + encodeURIComponent(dataToDownload);
+    downloadLink.download = "downloaded-data.txt";
+    downloadLink.style.display = "none";
+    
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    
+    document.body.removeChild(downloadLink);
+});
+
+
+// Get the copy, paste, and cut elements by their IDs
+const copyButton = document.getElementById("copy");
+const cutButton = document.getElementById("cut");
+const pasteButton = document.getElementById("paste");
+
+// Add event listener for the copy button
+copyButton.addEventListener("click", () => {
+    copyText();
+});
+
+// Add event listener for the cut button
+cutButton.addEventListener("click", () => {
+    cutText();
+});
+
+// Add event listener for the paste button
+pasteButton.addEventListener("click", () => {
+    pasteText();
+});
+
+function copyText() {
+    if (activeCell) {
+        const selectedText = window.getSelection().toString();
+        navigator.clipboard.writeText(selectedText).then(() => {
+            console.log("Text copied to clipboard: " + selectedText);
+        }).catch(err => {
+            console.error("Copy failed: " + err);
+        });
+    }
+}
+
+function cutText() {
+    if (activeCell) {
+        // 1. Copy the selected text to the clipboard.
+        const selectedText = window.getSelection().toString();
+        navigator.clipboard.writeText(selectedText).then(() => {
+            console.log("Text copied to clipboard: " + selectedText);
+
+            // 2. Clear the cell's content (remove the text).
+            activeCell.innerText = "";
+        }).catch(err => {
+            console.error("Cut failed: " + err);
+        });
+    }
+}
+
+
+function pasteText() {
+    if (activeCell) {
+        navigator.clipboard.readText().then(clipboardText => {
+            activeCell.innerText = clipboardText;
+            console.log("Text pasted from clipboard: " + clipboardText);
+        }).catch(err => {
+            console.error("Paste failed: " + err);
+        });
+    }
+}
+
+
+ 
